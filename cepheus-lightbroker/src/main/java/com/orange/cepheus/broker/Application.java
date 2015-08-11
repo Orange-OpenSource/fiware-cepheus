eca8e78e7064799fb9c861b2672834715d36c8d3
Marc Capdevielle <marc.capdevielle@orange.com>
2015-08-11 11:09:12 +0200

    add missing legal headers

--- cepheus-lightbroker/src/main/java/com/orange/cepheus/broker/Application.java
+++ cepheus-lightbroker/src/main/java/com/orange/cepheus/broker/Application.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2015 Orange
+ *
+ * This software is distributed under the terms and conditions of the 'GNU GENERAL PUBLIC LICENSE
+ * Version 2' license which can be found in the file 'LICENSE.txt' in this package distribution or
+ * at 'http://www.gnu.org/licenses/gpl-2.0-standalone.html'.
+ */
+
 package com.orange.cepheus.broker;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
