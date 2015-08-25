b052bc1256264d9f001596a4622b265518afc5e7
Pascale Borscia <pascale.borscia@orange.com>
2015-08-25 15:11:24 +0200

    broker and cep : add /NGSI10, /NGSI9 request mappings of controllers

--- cepheus-lightbroker/src/main/java/com/orange/cepheus/broker/controller/NgsiController.java
+++ cepheus-lightbroker/src/main/java/com/orange/cepheus/broker/controller/NgsiController.java
@@ -37,7 +37,7 @@ import java.util.stream.Collectors;
  * NGSI Controller : NGSI operation implemented by Cepheus-lightbroker
  */
 @RestController
-@RequestMapping(value = {"/v1","/ngsi9","/ngsi10"})
+@RequestMapping(value = {"/v1","/ngsi9","/NGSI9","/ngsi10","/NGSI10"})
 public class NgsiController extends NgsiBaseController {
 
     private static Logger logger = LoggerFactory.getLogger(NgsiController.class);
