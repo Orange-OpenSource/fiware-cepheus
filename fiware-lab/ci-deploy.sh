6a2a2e78454754b4454a51e35e09ad597ebab13f
Marc Capdevielle <marc.capdevielle@orange.com>
2015-09-23 18:02:18 +0200

    build: limit deployment to master branch
    - moved deployment to a dedicated script
    - deployment: deploy Sonatype snapshot and docker build

---
+++ fiware-lab/ci-deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+set -x
+
+# Deploy JAR to Sonatype Mave Repository
+mvn deploy -q -settings=settings.xml -DskipTests=true
+
+# Trigger Docker Automated build
+curl –s -H "Content-Type: application/json" --data "build=true" -X POST "https://registry.hub.docker.com/u/$DOCKER_USER/fiware-cepheus/trigger/$DOCKER_KEY/"
