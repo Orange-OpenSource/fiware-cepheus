beb320be969ea5c24a5a4fc0d0bcce223e2051d4
Marc Capdevielle <marc.capdevielle@orange.com>
2015-09-24 17:13:51 +0200

    add Bintray Debian repository upload on release

--- scripts/ci-release.sh
+++ scripts/ci-release.sh
@@ -9,3 +9,7 @@ set -x
 
 # Trigger Docker Automated build
 #curl -s -H "Content-Type: application/json" --data "build=true" -X POST "https://registry.hub.docker.com/u/$DOCKER_USER/fiware-cepheus/trigger/$DOCKER_KEY/"
+
+# Publish DEB packages to bintray.com Debian repository
+#curl -s -T "cepheus-cep/target/cepheus-cep_$TRAVIS_TAG_all.deb" -umarc4orange:$BINTRAY_KEY "https://api.bintray.com/content/orange-opensource/Fiware-Cepheus/Cepheus-CEP/$TRAVIS_TAG/cepheus-cep_$TRAVIS_TAG_all.deb;publish=1"
+#curl -s -T "cepheus-broker/target/cepheus-broker_$TRAVIS_TAG_all.deb" -umarc4orange:$BINTRAY_KEY "https://api.bintray.com/content/orange-opensource/Fiware-Cepheus/Cepheus-Broker/$TRAVIS_TAG/cepheus-broker_$TRAVIS_TAG_all.deb;publish=1"
