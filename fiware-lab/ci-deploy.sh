a7733fc937880779022487d37ba3849513f8e2d8
Pascale Borscia <pascale.borscia@orange.com>
2015-09-24 09:39:09 +0200

    build: fix docker trigger

--- fiware-lab/ci-deploy.sh
+++ fiware-lab/ci-deploy.sh
@@ -7,4 +7,4 @@ set -x
 mvn deploy -q -settings=settings.xml -DskipTests=true
 
 # Trigger Docker Automated build
-curl –s -H "Content-Type: application/json" --data "build=true" -X POST "https://registry.hub.docker.com/u/$DOCKER_USER/fiware-cepheus/trigger/$DOCKER_KEY/"
+curl -s -H "Content-Type: application/json" --data "build=true" -X POST "https://registry.hub.docker.com/u/$DOCKER_USER/fiware-cepheus/trigger/$DOCKER_KEY/"
