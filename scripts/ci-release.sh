43873980917d6adbe16ecc8d4f84775484ecb553
Pascale Borscia <pascale.borscia@orange.com>
2015-09-28 16:18:53 +0200

    build: launch the docker build trigger on the tag source

--- scripts/ci-release.sh
+++ scripts/ci-release.sh
@@ -8,7 +8,7 @@ set -x
 #mvn clean nexus-staging:release -Prelease -settings=settings.xml -DskipTests=true -Dgpg.passphrase=$GPG_PASSPHRASE -Dgpg.keyname=$GPG_KEYNAME
 
 # Trigger Docker Automated build
-#curl -s -H "Content-Type: application/json" --data "build=true" -X POST "https://registry.hub.docker.com/u/$DOCKER_USER/fiware-cepheus/trigger/$DOCKER_KEY/"
+#curl -s -H "Content-Type: application/json" --data '{"source_type": "Tag", "source_name": "0.1.1"}' -X POST "https://registry.hub.docker.com/u/$DOCKER_USER/fiware-cepheus/trigger/$DOCKER_KEY/"
 
 # Publish DEB packages to bintray.com Debian repository
 #curl -s -T "cepheus-cep/target/cepheus-cep_$TRAVIS_TAG_all.deb" -umarc4orange:$BINTRAY_KEY "https://api.bintray.com/content/orange-opensource/Fiware-Cepheus/Cepheus-CEP/$TRAVIS_TAG/cepheus-cep_$TRAVIS_TAG_all.deb;publish=1"
