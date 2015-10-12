283107bc1d7d59cfe5ade4e8e12d71f58b88849d
Pascale Borscia <pascale.borscia@orange.com>
2015-10-12 15:07:10 +0200

    broker: add subscriptions persistence
    - add org.xerial.sqlite dependancy
    - add spring-boot-starter-jdbc dependency
    - create SubscriptionsRepository which manages persistence methods
    - create SubscriptionsRepositoryTest

---
+++ cepheus-broker/src/main/java/com/orange/cepheus/broker/persistence/SubscriptionsRepository.java
@@ -0,0 +1,131 @@
+/*
+ * Copyright (C) 2015 Orange
+ *
+ * This software is distributed under the terms and conditions of the 'GNU GENERAL PUBLIC LICENSE
+ * Version 2' license which can be found in the file 'LICENSE.txt' in this package distribution or
+ * at 'http://www.gnu.org/licenses/gpl-2.0-standalone.html'.
+ */
+
+package com.orange.cepheus.broker.persistence;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.orange.ngsi.model.SubscribeContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Repository for Subscriptions
+ */
+@Repository
+public class SubscriptionsRepository {
+    private static Logger logger = LoggerFactory.getLogger(SubscriptionsRepository.class);
+
+    @Autowired
+    protected JdbcTemplate jdbcTemplate;
+
+    private static class Subscription {
+        String subscriptionId;
+        String subscribeContextString;
+
+        public Subscription() {
+        }
+
+        public void setSubscriptionId(String subscriptionId) {
+            this.subscriptionId = subscriptionId;
+        }
+
+        public void setSubscribeContextString(String subscribeContextString) {
+            this.subscribeContextString = subscribeContextString;
+        }
+
+        public String getSubscriptionId() {
+            return subscriptionId;
+        }
+
+        public String getSubscribeContextString() {
+            return subscribeContextString;
+        }
+    }
+
+    /**
+     * Save a subscription.
+     * @param subscriptionId
+     * @param subscribeContext
+     * @throws JsonProcessingException
+     */
+    public void saveSubscription(String subscriptionId, SubscribeContext subscribeContext) throws JsonProcessingException {
+        //serialization
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectWriter writer = mapper.writer(new DefaultPrettyPrinter());
+        String susbcribeContextString = writer.writeValueAsString(subscribeContext);
+
+        jdbcTemplate.update("insert into t_subscriptions(id,subscribeContext) values(?,?)", subscriptionId, susbcribeContextString);
+    }
+
+    /**
+     * Save a subscription updated
+     * @param subscriptionId
+     * @param subscribeContext
+     * @throws JsonProcessingException
+     */
+    public void updateSubscription(String subscriptionId, SubscribeContext subscribeContext) throws JsonProcessingException {
+        //serialization
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectWriter writer = mapper.writer();
+        String susbcribeContextString = writer.writeValueAsString(subscribeContext);
+
+        jdbcTemplate.update("update t_subscriptions set subscribeContext=? where id=?", susbcribeContextString, subscriptionId);
+    }
+
+    /**
+     * Get all subscriptions saved
+     * @return subscriptions map
+     */
+    public Map<String, SubscribeContext> getAllSubscriptions() {
+        Map<String, SubscribeContext> subscriptions = new ConcurrentHashMap<>();
+        List<Subscription> subscriptionList = jdbcTemplate.query( "select id, subscribeContext from t_subscriptions", new SubscriptionMapper());
+        ObjectMapper mapper = new ObjectMapper();
+        subscriptionList.forEach(subscription -> {
+            try {
+                SubscribeContext subscribeContext = mapper.readValue(subscription.getSubscribeContextString(), SubscribeContext.class);
+                subscriptions.put(subscription.getSubscriptionId(), subscribeContext);
+            } catch (IOException e) {
+                logger.warn("failed to get subscription {}", subscription.getSubscribeContextString());
+            }
+        });
+        return subscriptions;
+    }
+
+    /**
+     * Remove a subscription.
+     * @param subscriptionId
+     */
+    public void removeSubscription(String subscriptionId) {
+        jdbcTemplate.update("delete from t_subscriptions where id=?", subscriptionId);
+    }
+
+    private static final class SubscriptionMapper implements RowMapper<Subscription> {
+
+        public Subscription mapRow(ResultSet rs, int rowNum) throws SQLException {
+            Subscription subscription = new Subscription();
+            subscription.setSubscriptionId(rs.getString("id"));
+            subscription.setSubscribeContextString(rs.getString("subscribeContext"));
+            return subscription;
+        }
+    }
+
+}
---
+++ cepheus-broker/src/test/java/com/orange/cepheus/broker/persistence/SubscriptionsRepositoryTest.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (C) 2015 Orange
+ *
+ * This software is distributed under the terms and conditions of the 'GNU GENERAL PUBLIC LICENSE
+ * Version 2' license which can be found in the file 'LICENSE.txt' in this package distribution or
+ * at 'http://www.gnu.org/licenses/gpl-2.0-standalone.html'.
+ */
+
+package com.orange.cepheus.broker.persistence;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.orange.cepheus.broker.Application;
+import com.orange.ngsi.model.SubscribeContext;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.net.URISyntaxException;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Map;
+
+import static com.orange.cepheus.broker.Util.createSubscribeContextTemperature;
+
+/**
+ * Tests for SubscriptionsRepository
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+//@TestPropertySource(locations="classpath:test.properties")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class SubscriptionsRepositoryTest {
+
+    @Autowired
+    SubscriptionsRepository subscriptionsRepository;
+
+    @Autowired
+    protected JdbcTemplate jdbcTemplate;
+
+    @Before
+    public void init() throws SQLException {
+        /*DatabaseMetaData md = jdbcTemplate.getDataSource().getConnection().getMetaData();
+        ResultSet rs = md.getTables(null, null, "t_subscriptions", null);
+        if (rs.next()) {
+            jdbcTemplate.execute("drop table t_subscriptions");
+        }*/
+        jdbcTemplate.execute("drop table if exists t_subscriptions");
+        jdbcTemplate.execute("create table t_subscriptions (id varchar, subscribeContext varchar)");
+    }
+
+    @Test
+    public void saveSubscriptionTest() throws URISyntaxException, JsonProcessingException {
+        SubscribeContext subscribeContext = createSubscribeContextTemperature();
+        subscriptionsRepository.saveSubscription("12345", subscribeContext);
+        Map<String, SubscribeContext> subscriptions = subscriptionsRepository.getAllSubscriptions();
+        Assert.assertEquals(1, subscriptions.size());
+        Assert.assertEquals(subscribeContext.getDuration(), subscriptions.get("12345").getDuration());
+    }
+
+    @Test
+    public void updateSubscriptionTest() throws URISyntaxException, JsonProcessingException {
+        SubscribeContext subscribeContext = createSubscribeContextTemperature();
+        subscriptionsRepository.saveSubscription("12345", subscribeContext);
+        Map<String, SubscribeContext> subscriptions = subscriptionsRepository.getAllSubscriptions();
+        Assert.assertEquals(1, subscriptions.size());
+        Assert.assertEquals("P1M", subscriptions.get("12345").getDuration());
+        subscribeContext.setDuration("PT10S");
+        subscriptionsRepository.updateSubscription("12345", subscribeContext);
+        subscriptions = subscriptionsRepository.getAllSubscriptions();
+        Assert.assertEquals(1, subscriptions.size());
+        Assert.assertEquals("PT10S", subscriptions.get("12345").getDuration());
+
+    }
+
+    @Test
+    public void removeSubscriptionTest() throws URISyntaxException, JsonProcessingException {
+        SubscribeContext subscribeContext = createSubscribeContextTemperature();
+        subscriptionsRepository.saveSubscription("12345", subscribeContext);
+        Assert.assertEquals(1, subscriptionsRepository.getAllSubscriptions().size());
+        subscriptionsRepository.removeSubscription("12345");
+        Assert.assertEquals(0, subscriptionsRepository.getAllSubscriptions().size());
+    }
+
+    @Test
+    public void getAllSubscriptionsTest() throws URISyntaxException, JsonProcessingException {
+        SubscribeContext subscribeContext = createSubscribeContextTemperature();
+        subscriptionsRepository.saveSubscription("12345", subscribeContext);
+        subscriptionsRepository.saveSubscription("12346", subscribeContext);
+        subscriptionsRepository.saveSubscription("12347", subscribeContext);
+        Assert.assertEquals(3, subscriptionsRepository.getAllSubscriptions().size());
+    }
+
+}
