04ab31e7a6c3b659ac20851913106c6961d31e32
Marc Capdevielle <marc.capdevielle@orange.com>
2015-10-29 15:43:26 +0100

    scripts: fix pre-release.sh script
    - overwrite "release" branch before start if any
    - use version from pom.xml
    - add changes before commit
    - remove "release" after tag

--- scripts/pre-release.sh
+++ scripts/pre-release.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 # This script prepare the workspace and git for the release build
 
-# Require the version argument
-if (( "$#" < "1" )); then
-    echo "Usage: pre-release.sh version"
-    echo "Example: pre-release.sh 0.1.1"
-    exit 1
-fi
-
 set -e
 set -x
 
-VERSION="$1"
+VERSION=`cat pom.xml | grep "<version>" | head -1 | sed 's/[^0-9.]*\([0-9.]*\).*/\1/'`
 
 # Prepare workspace
+git checkout master
 git pull origin master
-git checkout -b release
+git branch -f release master
+git checkout release
 
 # Replace {version}-SNAPSHOT by {version} except ci-release.sh
 find . -name "*.xml" -type f -exec sed -i '' "s/-SNAPSHOT//g" {} \;
@@ -29,6 +24,10 @@ find . -name "Dockerfile" -type f -exec sed -i '' "s/snapshots/releases/g" {} \;
 find . -name "*.sh" ! -name 'ci-release.sh'  -type f -exec sed -i '' "s/snapshots/releases/g" {} \;
 
 # Push modifications
-git commit -m "prepare $VERSION"
+git commit -a -m "prepare $VERSION"
 git tag $VERSION
 git push origin $VERSION
+
+# Switch back to master
+git checkout master
+git branch -D release
