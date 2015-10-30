3fa275e5a1e50fb0ad2399b36d421c24a4594685
Pascale Borscia <pascale.borscia@orange.com>
2015-10-30 14:49:04 +0100

    build: integration in our CI
    - suppress git commands
    - factorisation find commands

--- scripts/post-release.sh
+++ scripts/post-release.sh
@@ -2,28 +2,19 @@
 # This script prepare the workspace and git after the release build
 
 # Require the version argument
-if (( "$#" < "2" )); then
-    echo "Usage: post-release.sh old-version new-version"
-    echo "Example: post-release.sh 0.1.1 0.1.2"
+if (( "$#" < "1" )); then
+    echo "Usage: post-release.sh new-version"
+    echo "Example: post-release.sh 0.1.0"
     exit 1
 fi
 
 set -e
 set -x
 
-OLDVERSION="$1"
-NEWVERSION="$2"
-
-# Prepare master to the new version
-git checkout master
-git branch -D release
+OLDVERSION=`cat pom.xml | grep "<version>" | head -1 | sed 's/[^0-9.]*\([0-9.]*\).*/\1/'`
+NEWVERSION="$1"
 
 # Replace {version}-SNAPSHOT by {NEWVERSION} except ci-release.sh
-find . -name "*.xml" -type f -exec sed -i '' "s/$OLDVERSION-SNAPSHOT/$NEWVERSION-SNAPSHOT/g" {} \;
-find . -name "*.md" -type f -exec sed -i '' "s/$OLDVERSION-SNAPSHOT/$NEWVERSION-SNAPSHOT/g" {} \;
-find . -name "Dockerfile" -type f -exec sed -i '' "s/$OLDVERSION-SNAPSHOT/$NEWVERSION-SNAPSHOT/g" {} \;
-find . -name "*.sh" ! -name 'ci-release.sh'  -type f -exec sed -i '' "s/$OLDVERSION-SNAPSHOT/$NEWVERSION-SNAPSHOT/g" {} \;
+mvn versions:set -DnewVersion=$NEWVERSION-SNAPSHOT -DgenerateBackupPoms=false
 
-# Push modifications
-git commit -m "prepare $NEWVERSION"
-git push origin master
+find . -name "Dockerfile" -name "*.md"  -name "*.sh" ! -name 'ci-release.sh'  -type f -exec sed -i '' "s/$OLDVERSION-SNAPSHOT/$NEWVERSION-SNAPSHOT/g" {} \;
--- scripts/pre-release.sh
+++ scripts/pre-release.sh
@@ -1,33 +1,10 @@
 #!/bin/bash
 # This script prepare the workspace and git for the release build
-
 set -e
 set -x
 
-VERSION=`cat pom.xml | grep "<version>" | head -1 | sed 's/[^0-9.]*\([0-9.]*\).*/\1/'`
-
-# Prepare workspace
-git checkout master
-git pull origin master
-git branch -f release master
-git checkout release
-
 # Replace {version}-SNAPSHOT by {version} except ci-release.sh
-find . -name "*.xml" -type f -exec sed -i '' "s/-SNAPSHOT//g" {} \;
-find . -name "*.md" -type f -exec sed -i '' "s/-SNAPSHOT//g" {} \;
-find . -name "Dockerfile" -type f -exec sed -i '' "s/-SNAPSHOT//g" {} \;
-find . -name "*.sh" ! -name 'ci-release.sh'  -type f -exec sed -i '' "s/-SNAPSHOT//g" {} \;
+find . -name "*.xml" -name "Dockerfile" -name "*.md" -name "*.sh" ! -name 'ci-release.sh'  -type f -exec sed -i '' "s/-SNAPSHOT//g" {} \;
 
 # Replace snapshots by releases except in .xml files
-find . -name "*.md" -type f -exec sed -i '' "s/snapshots/releases/g" {} \;
-find . -name "Dockerfile" -type f -exec sed -i '' "s/snapshots/releases/g" {} \;
-find . -name "*.sh" ! -name 'ci-release.sh'  -type f -exec sed -i '' "s/snapshots/releases/g" {} \;
-
-# Push modifications
-git commit -a -m "prepare $VERSION"
-git tag $VERSION
-git push origin $VERSION
-
-# Switch back to master
-git checkout master
-git branch -D release
+find . -name "Dockerfile" -name "*.md" -name "*.sh" ! -name 'ci-release.sh'  -type f -exec sed -i '' "s/snapshots/releases/g" {} \;
