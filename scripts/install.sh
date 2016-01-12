e227ee415f35dead3d177ac9147893923a2710da
Pascale Borscia <pascale.borscia@orange.com>
2016-01-12 11:09:59 +0100

    script: launch cepheus on boot of the Fiware Lab VM

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Setup Cepheus-CEP and Cepheus-Broker on Ubuntu 14.04 (trusty)
+# Setup Cepheus-CEP and Cepheus-Broker on Ubuntu 14.04 for Fiware Lab
 
 # Cepheus version
 REPO="releases"
@@ -8,10 +8,6 @@ VERSION="LATEST"
 # Check and echo all commands
 set -e
 set -x
-
-# Update the machine was already done
-#sudo apt-get update
-#sudo apt-get upgrade -y
 
 # Install curl and add-apt-repository
 sudo apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" curl
@@ -28,9 +24,18 @@ sudo apt-get install -q -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options
 curl -L -o cepheus-cep.jar "http://oss.sonatype.org/service/local/artifact/maven/redirect?r=$REPO&g=com.orange.cepheus&a=cepheus-cep&v=$VERSION&p=jar"
 curl -L -o cepheus-broker.jar "http://oss.sonatype.org/service/local/artifact/maven/redirect?r=$REPO&g=com.orange.cepheus&a=cepheus-broker&v=$VERSION&p=jar"
 
-# launch Cepheus-CEP and Cepheus-Broker
-nohup java -jar -Djava.security.egd=file:/dev/./urandom cepheus-cep.jar --logging.config=file --logging.file=cep.log --port=8080 2>> /dev/null >> /dev/null &
-nohup java -jar -Djava.security.egd=file:/dev/./urandom cepheus-broker.jar --spring.datasource.url=jdbc:sqlite:cepheus-broker.db --logging.config=file --logging.file=broker.log --port=8081 2>> /dev/null >> /dev/null &
+# create launcher script
+echo '#!/bin/bash' > launcher-cepheus.sh
+echo 'nohup java -jar -Djava.security.egd=file:/dev/./urandom cepheus-cep.jar --logging.config=file --logging.file=cep.log --port=8080 2>> /dev/null &' >> launcher-cepheus.sh
+echo 'nohup java -jar -Djava.security.egd=file:/dev/./urandom cepheus-broker.jar --spring.datasource.url=jdbc:sqlite:cepheus-broker.db --logging.config=file --logging.file=broker.log --port=8081 2>> /dev/null >> /dev/null &' >> launcher-cepheus.sh
+echo 'exit 0' >> launcher-cepheus.sh
+
+sudo chmod 777 launcher-cepheus.sh
+
+# launch Cepheus-CEP and Cepheus-Broker on boot
+sudo sed -i -e '$i \/home/ubuntu/launcher-cepheus.sh\n' /etc/rc.local
+
+
 
 
 
