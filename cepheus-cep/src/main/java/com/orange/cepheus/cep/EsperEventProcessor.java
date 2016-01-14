Orange-OpenSource/fiware-cepheus

commit 7f623d20461c94f0b01a9c959af696849f505846

cep: fix variable updates not applying #19
we have to keep a list of statements declaring variables
and remove the statements using variables on statement update.

cepheus-cep/src/main/java/com/orange/cepheus/cep/EsperEventProcessor.java
@@ -9,6 +9,7 @@
 package com.orange.cepheus.cep;
 
 import com.espertech.esper.client.*;
+import com.espertech.esper.client.soda.EPStatementObjectModel;
 import com.orange.cepheus.cep.exception.ConfigurationException;
 import com.orange.cepheus.cep.exception.EventProcessingException;
 import com.orange.cepheus.cep.exception.EventTypeNotFoundException;
@@ -36,6 +37,12 @@ public class EsperEventProcessor implements ComplexEventProcessor {
 
     private final EPServiceProvider epServiceProvider;
     private Configuration configuration;
+
+    /**
+     * Keep a list of statements that declare a variable (create variable),
+     * required to properly remove them on updates as Esper does not provide this.
+     */
+    private HashMap<String, String> variablesByStatementName = new HashMap<>();
 
     @Autowired
     public EventMapper eventMapper;
@@ -103,6 +110,9 @@ public class EsperEventProcessor implements ComplexEventProcessor {
             for (com.espertech.esper.client.EventType eventType : operations.getEventTypes()) {
                 operations.removeEventType(eventType.getName(), true);
             }
+
+            // Remove all statements to variables associations
+            variablesByStatementName.clear();
 
             // Adding back in/out events, then statements
             Collection<EventType> inEventTypes = Collections.unmodifiableList(previousConfiguration.getEventTypeIns());
@@ -225,30 +235,34 @@ public class EsperEventProcessor implements ComplexEventProcessor {
     private void updateStatements(Collection<String> statements) throws NoSuchAlgorithmException {
         // Keep a list of MD5 hash of all added statements
         Set<String> hashes = new HashSet<>();
+        for (String eplStatement : statements) {
+            hashes.add(MD5(eplStatement));
+        }
+
+        // Removed unused statements first
+        for (String hash : epServiceProvider.getEPAdministrator().getStatementNames()) {
+            if (!hashes.contains(hash)) {
+                removeStatement(hash);
+            }
+        }
 
         // Update EPL statements
         for (String eplStatement : statements) {
             String hash = MD5(eplStatement);
-            hashes.add(hash);
 
             // Create statement if does not already exist
             EPStatement statement = epServiceProvider.getEPAdministrator().getStatement(hash);
             if (statement == null) {
                 logger.info("Add new statement: {}", eplStatement);
-                statement = epServiceProvider.getEPAdministrator().createEPL(eplStatement, hash);
-                statement.addListener(eventSinkListener);
-            }
+                EPStatementObjectModel model = epServiceProvider.getEPAdministrator().compileEPL(eplStatement);
 
-        }
-
-        // Removed unused statements
-        for (String hash : epServiceProvider.getEPAdministrator().getStatementNames()) {
-            if (!hashes.contains(hash)) {
-                EPStatement statement = epServiceProvider.getEPAdministrator().getStatement(hash);
-                if (statement != null) {
-                    logger.info("Remove unused statement: {}", statement.getText());
-                    statement.destroy();
+                // When the statement defines a new variable, keep track of this association
+                if (model.getCreateVariable() != null) {
+                    variablesByStatementName.put(hash, model.getCreateVariable().getVariableName());
                 }
+
+                statement = epServiceProvider.getEPAdministrator().create(model);
+                statement.addListener(eventSinkListener);
             }
         }
     }
@@ -268,5 +282,30 @@ public class EsperEventProcessor implements ComplexEventProcessor {
             sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1,3));
         }
         return sb.toString();*/
+    }
+
+    /**
+     * Remove a statement, recursively removing statements depending on it
+     * when it is a statement declaring a variable
+     * @param statementName the hash of the statement to delete
+     */
+    private void removeStatement(String statementName) {
+        EPStatement statement = epServiceProvider.getEPAdministrator().getStatement(statementName);
+        if (statement != null) {
+            logger.info("Remove statement: {}", statement.getText());
+
+            // Destroy all statements associated to a given statement declaring a variable
+            String variableName = variablesByStatementName.get(statementName);
+            if (variableName != null) {
+                Set<String> epStatements = epServiceProvider.getEPAdministrator().getConfiguration().getVariableNameUsedBy(variableName);
+                for (String epStatement : epStatements) {
+                    removeStatement(epStatement);
+                }
+            }
+
+            //TODO support other type of statement dependency ?
+
+            statement.destroy();
+        }
     }
 }

cepheus-cep/src/test/java/com/orange/cepheus/cep/EsperEventProcessorTest.java
@@ -27,6 +27,7 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import java.util.*;
 
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.fail;
 import static org.mockito.Mockito.*;
 
@@ -185,6 +186,31 @@ public class EsperEventProcessorTest {
         assertEquals(false, esperEventProcessor.restoreConfiguration(badConfiguration));
     }
 
+    /**
+     * Check that we correctly handle updating variable in configuration
+     * @throws Exception
+     */
+    @Test
+    public void checkVariableUpdate() throws Exception {
+        Configuration configuration = emptyConfiguration();
+        List<String> statements = new ArrayList<>();
+        statements.add("create variable int i = 1");
+        statements.add("create variable int j = i");
+        configuration.setStatements(statements);
+        esperEventProcessor.setConfiguration(configuration);
+
+        Configuration configuration2 = emptyConfiguration();
+        List<String> statements2 = new ArrayList<>();
+        statements2.add("create variable int i = 2");
+        statements2.add("create variable int j = i");
+        configuration2.setStatements(statements2);
+        esperEventProcessor.setConfiguration(configuration2);
+
+        assertEquals(2, esperEventProcessor.getStatements().size());
+        for (String statement : esperEventProcessor.getStatements()) {
+            assertNotEquals("create variable int i = 1", statement);
+        }
+    }
 
     /**
      * Check that the update listener is called on when an event is generated by the Esper CEP engine

cepheus-cep/src/test/java/com/orange/cepheus/cep/Util.java
@@ -55,7 +55,7 @@ public class Util {
 
         //rules
         List<String> rules = new ArrayList<>();
-        rules.add("INSERT INTO TempSensorAvg SELECT 'OUT1' as id, avg(temp) as avgTemp, temp_unit as avgTemp_unit FROM TempSensor.win:time(2 seconds) WHERE TempSensor.id = 'S1' ");
+        rules.add("insert into TempSensorAvg select \"OUT1\" as id, avg(temp) as avgTemp, temp_unit as avgTemp_unit from TempSensor.win:time(2 seconds) where TempSensor.id=\"S1\"");
         configuration.setStatements(rules);
 
         return configuration;

commit 1b606fd9e6afa391ba1d0d8c71cb0e40748e73df

cep: fix #36 systematic recreation of statement on configuration update
regression introduces in 7f623d20461c94f0b01a9c959af696849f505846 (1.2.0)

cepheus-cep/src/main/java/com/orange/cepheus/cep/EsperEventProcessor.java
@@ -289,7 +289,7 @@ public class EsperEventProcessor implements ComplexEventProcessor {
                     variablesByStatementName.put(hash, model.getCreateVariable().getVariableName());
                 }
 
-                statement = epServiceProvider.getEPAdministrator().create(model);
+                statement = epServiceProvider.getEPAdministrator().create(model, hash);
                 statement.addListener(eventSinkListener);
             }
         }
@@ -326,8 +326,10 @@ public class EsperEventProcessor implements ComplexEventProcessor {
             String variableName = variablesByStatementName.get(statementName);
             if (variableName != null) {
                 Set<String> epStatements = epServiceProvider.getEPAdministrator().getConfiguration().getVariableNameUsedBy(variableName);
-                for (String epStatement : epStatements) {
-                    removeStatement(epStatement);
+                for (String epStatement : new LinkedList<>(epStatements)) { // use a new list to prevent conccurent access
+                    if (!epStatement.equals(statementName)) {
+                        removeStatement(epStatement);
+                    }
                 }
             }
 